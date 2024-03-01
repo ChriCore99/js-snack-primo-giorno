1544738d5183b97dcfed4903698d1934345d2858
ChriCore99 <christian.corelli.io@gmail.com>
2024-03-01 15:33:38 +0100

    ADD: completamento linee codice esercizio

---
+++ js/javascript.js
@@ -0,0 +1,39 @@
+// esercizio 1
+
+let number = 100; //number
+
+console.log(number); // number
+
+// esercizio 2
+
+const PI = 3.14; //number
+
+console.log(PI); //number
+
+// esercizio 2-B
+
+// PI = 3.1416; non si puo cambiare il valore di una costante in questo modo essendo una costante
+
+// (se devo farlo potrei fare così)
+
+const PI2 = "16"; //stringa
+
+let PIChange = PI + PI2; //string
+
+console.log(PIChange); //string
+
+// esercizio 2 - C
+
+let radius = 8; //number
+
+let circle = (radius * 2) * PI; //number
+
+console.log(circle); //number
+
+// esercizio 3
+
+let name = "carlo"; //string
+
+name = "marco"; //string
+
+console.log(name); //string
